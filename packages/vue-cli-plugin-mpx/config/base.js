224fcfc2e0723343958681154d15fb86ed376918
xiaolei <xiaolei@didichuxing.com>
2020-12-08 15:38:12 +0800

    [feat]plugin-mpx内集成transpileDependencies功能

--- packages/vue-cli-plugin-mpx/config/base.js
+++ packages/vue-cli-plugin-mpx/config/base.js
@@ -25,10 +25,12 @@ module.exports = function (api, options, webpackConfig) {
     .loader(mpxUrlLoader.loader)
     .options(mpxUrlLoader.options)
 
+  const transpileDepRegex = genTranspileDepRegex(options.transpileDependencies)
   webpackConfig.module
     .rule('js')
     .test(/\.js$/)
     .include
+      .add(filepath => transpileDepRegex && transpileDepRegex.test(filepath))
       .add(api.resolve('src'))
       .add(api.resolve('node_modules/@mpxjs'))
       .add(api.resolve('test'))
@@ -43,4 +45,20 @@ module.exports = function (api, options, webpackConfig) {
     .add('.js')
 
   webpackConfig.resolve.modules.add('node_modules')
+}
+
+function genTranspileDepRegex(transpileDependencies) {
+  const path = require('path')
+  const { isWindows } = require('@vue/cli-shared-utils')
+  const deps = transpileDependencies.map((dep) => {
+    if (typeof dep === 'string') {
+      const depPath = path.join('node_modules', dep, '/')
+      return isWindows
+        ? depPath.replace(/\\/g, '\\\\') // double escape for windows style path
+        : depPath
+    } else if (dep instanceof RegExp) {
+      return dep.source
+    }
+  })
+  return deps.length ? new RegExp(deps.join('|')) : null
 }
