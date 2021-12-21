9549f20045f17c20713b963147b73d54d35df564
Mater <bxh8640@gmail.com>
2021-12-21 15:35:22 +0800

    fix: use exist test project

--- scripts/test.js
+++ scripts/test.js
@@ -2,17 +2,19 @@ const execa = require('execa')
 const glob = require('glob')
 const path = require('path')
 const ora = require('ora')
+const fs = require('fs')
 
 async function run(project) {
   const cwd = process.cwd()
-  const resolvePackages = (p = '') => path.join(cwd, 'packages', p)
-  const packages = glob.sync('*', { cwd: resolvePackages() })
-  await removeDir(project)
-  await execa('node', ['packages/mpx-cli/bin/mpx.js', 'create', project], {
-    stdio: 'inherit'
-  })
   const projectRoot = path.join(cwd, project)
   const resolveProject = (dir = '') => path.join(projectRoot, dir)
+  const resolvePackages = (p = '') => path.join(cwd, 'packages', p)
+  const packages = glob.sync('*', { cwd: resolvePackages() })
+  if (!fs.existsSync(projectRoot)) {
+    await execa('node', ['packages/mpx-cli/bin/mpx.js', 'create', project], {
+      stdio: 'inherit'
+    })
+  }
   const installedPackages = glob.sync('*', {
     cwd: resolveProject('node_modules/@mpxjs')
   })
