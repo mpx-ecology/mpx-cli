34c367bd58c515a04dfcf65bd4333ec31a6f0ea6
Mater <bxh8640@gmail.com>
2022-01-28 17:45:42 +0800

    fix: remove mpx-mp dep

--- packages/vue-cli-plugin-mpx-mp/generator/index.js
+++ packages/vue-cli-plugin-mpx-mp/generator/index.js
@@ -1,10 +1,3 @@
 module.exports = function (api, options = {}) {
-  api.extendPackage({
-    devDependencies: {
-      'html-loader': '^3.0.1',
-      'css-loader': '^0.28.11',
-      stylus: '^0.55.0',
-      "stylus-loader": "^3.0.2"
-    }
-  })
+  api.extendPackage({})
 }
