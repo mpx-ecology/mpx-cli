9d3954a8ece263c3596619f35c8414b23751bea5
Mater <bxh8640@gmail.com>
2022-01-28 17:28:01 +0800

    fix: remove vue-cli-plugin-mpx-web de

--- packages/vue-cli-plugin-mpx-web/generator/index.js
+++ packages/vue-cli-plugin-mpx-web/generator/index.js
@@ -4,13 +4,6 @@ module.exports = function(api, options) {
     scripts: {
       'watch:web': 'MPX_CLI_MODE=web mpx-cli-service serve:web',
       'build:web': 'MPX_CLI_MODE=web mpx-cli-service build:web'
-    },
-    devDependencies: {
-      'html-loader': '^3.0.1',
-      'css-loader': '^0.28.11',
-      stylus: '^0.54.5',
-      'stylus-loader': '^3.0.2',
-      "vue-style-loader": "^4.1.2",
     }
   })
 }
