5954cf24841c00b25abcdeed4fb52cc9fdf21152
Mater <bxh8640@gmail.com>
2022-02-11 15:25:36 +0800

    refactor: 固定mpx-cli plugins version

--- packages/mpx-cli/lib/plugins.js
+++ packages/mpx-cli/lib/plugins.js
@@ -2,16 +2,24 @@ const prefix = '@mpxjs/vue-cli-plugin-mpx'
 
 const plugins = {
   tsSupport: {
-    [`${prefix}-typescript`]: {}
+    [`${prefix}-typescript`]: {
+      version: '^1.1.0-beta.1'
+    }
   },
   cloudFunc: {
-    [`${prefix}-cloud-func`]: {}
+    [`${prefix}-cloud-func`]: {
+      version: '^1.1.0-beta.1'
+    }
   },
   isPlugin: {
-    [`${prefix}-plugin-mode`]: {}
+    [`${prefix}-plugin-mode`]: {
+      version: '^1.1.0-beta.1'
+    }
   },
   transWeb: {
-    [`${prefix}-web`]: {}
+    [`${prefix}-web`]: {
+      version: '^1.1.0-beta.1'
+    }
   }
   // TODO: 添加其他插件配置
 }
--- packages/mpx-cli/lib/preset.js
+++ packages/mpx-cli/lib/preset.js
@@ -1,9 +1,17 @@
 module.exports = {
   cssPreprocessor: 'stylus',
   plugins: {
-    '@mpxjs/mpx-cli-service': {},
-    '@mpxjs/vue-cli-plugin-mpx': {},
-    '@mpxjs/vue-cli-plugin-mpx-mp': {},
-    '@mpxjs/vue-cli-plugin-mpx-eslint': {}
+    '@mpxjs/mpx-cli-service': {
+      version: '^1.1.0-beta.1'
+    },
+    '@mpxjs/vue-cli-plugin-mpx': {
+      version: '^1.1.0-beta.1'
+    },
+    '@mpxjs/vue-cli-plugin-mpx-mp': {
+      version: '^1.1.0-beta.1'
+    },
+    '@mpxjs/vue-cli-plugin-mpx-eslint': {
+      version: '^1.1.0-beta.1'
+    }
   }
 }
