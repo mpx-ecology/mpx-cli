f5ebb6ab27af763281698db26a1972d796315f48
Mater <bxh8640@gmail.com>
2022-02-14 12:34:55 +0800

    fix: mpx-cli 插件版本

--- packages/mpx-cli/lib/plugins.js
+++ packages/mpx-cli/lib/plugins.js
@@ -3,22 +3,22 @@ const prefix = '@mpxjs/vue-cli-plugin-mpx'
 const plugins = {
   tsSupport: {
     [`${prefix}-typescript`]: {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     }
   },
   cloudFunc: {
     [`${prefix}-cloud-func`]: {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     }
   },
   isPlugin: {
     [`${prefix}-plugin-mode`]: {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     }
   },
   transWeb: {
     [`${prefix}-web`]: {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     }
   }
   // TODO: 添加其他插件配置
--- packages/mpx-cli/lib/preset.js
+++ packages/mpx-cli/lib/preset.js
@@ -2,16 +2,16 @@ module.exports = {
   cssPreprocessor: 'stylus',
   plugins: {
     '@mpxjs/mpx-cli-service': {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     },
     '@mpxjs/vue-cli-plugin-mpx': {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     },
     '@mpxjs/vue-cli-plugin-mpx-mp': {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     },
     '@mpxjs/vue-cli-plugin-mpx-eslint': {
-      version: '^1.1.0-beta.1'
+      version: '^1.0.0'
     }
   }
 }
