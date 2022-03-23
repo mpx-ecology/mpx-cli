15bb5959d3f7567e36b59b93902e8513b340c88e
Mater <bxh8640@gmail.com>
2022-03-23 19:19:20 +0800

    Fix: build:web modern mode error (#13)
    * fix: module判断逻辑

--- packages/vue-cli-plugin-mpx-web/commands/buildWeb.js
+++ packages/vue-cli-plugin-mpx-web/commands/buildWeb.js
@@ -1,7 +1,40 @@
-module.exports = function(api, options) {
+module.exports = function (api, options) {
   const { build } = api.service.commands
 
-  api.registerCommand('build:web', async function (...args) {
-    return build.fn(...args)
+  async function legacyBuild(args, ...p) {
+    process.env.VUE_CLI_MODERN_BUILD = false
+    process.env.VUE_CLI_MODERN_MODE = true
+    await build.fn(
+      {
+        ...args,
+        module: false,
+        moduleBuild: false,
+        keepAlive: true
+      },
+      ...p
+    )
+  }
+
+  async function moduleBuild(args, ...p) {
+    process.env.VUE_CLI_MODERN_BUILD = true
+    process.env.VUE_CLI_MODERN_MODE = true
+    await build.fn(
+      {
+        ...args,
+        module: true,
+        moduleBuild: true,
+        clean: false
+      },
+      ...p
+    )
+  }
+
+  api.registerCommand('build:web', async function (args, ...p) {
+    if (args.module === false) {
+      return build.fn(args, ...p)
+    } else {
+      await legacyBuild({ ...args }, ...p)
+      await moduleBuild({ ...args }, ...p)
+    }
   })
 }
