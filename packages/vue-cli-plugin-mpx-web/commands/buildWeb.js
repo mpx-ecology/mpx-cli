7f93cde14125559f8c68e856e803ed9aacdcbe18
Mater <bxh8640@gmail.com>
2022-03-23 11:14:32 +0800

    fix: modern build

--- packages/vue-cli-plugin-mpx-web/commands/buildWeb.js
+++ packages/vue-cli-plugin-mpx-web/commands/buildWeb.js
@@ -1,8 +1,15 @@
-module.exports = function(api, options) {
+module.exports = function (api, options) {
   const { build } = api.service.commands
 
   api.registerCommand('build:web', async function (args, ...p) {
-    args.module = args.module || false
-    return build.fn(args, ...p)
+    if (!args.module) {
+      return build.fn(args, ...p)
+    } else {
+      args.module = false
+      await build.fn(args, ...p)
+      process.env.VUE_CLI_MODERN_BUILD = true
+      process.env.VUE_CLI_MODERN_MODE = true
+      await build.fn(args, ...p)
+    }
   })
 }
