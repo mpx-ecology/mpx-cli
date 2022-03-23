e8141d5b7b1e36b22982e06964f583403ce0f94d
Mater <bxh8640@gmail.com>
2022-03-23 16:26:07 +0800

    fix: build web modern error

--- packages/vue-cli-plugin-mpx-web/commands/buildWeb.js
+++ packages/vue-cli-plugin-mpx-web/commands/buildWeb.js
@@ -1,15 +1,40 @@
 module.exports = function (api, options) {
   const { build } = api.service.commands
 
+  async function legacyBuild(args, ...p) {
+    process.env.VUE_CLI_MODERN_BUILD = false
+    process.env.VUE_CLI_MODERN_MODE = true
+    await build.fn(
+      {
+        ...args,
+        module: false,
+        moduleBuild: false,
+        keepAlive: true
+      },
+      ...p
+    )
+  }
+
+  async function moduleBuild(args, ...p) {
+    process.env.VUE_CLI_MODERN_BUILD = true
+    process.env.VUE_CLI_MODERN_MODE = true
+    await build.fn(
+      {
+        ...args,
+        module: true,
+        moduleBuild: true,
+        clean: false
+      },
+      ...p
+    )
+  }
+
   api.registerCommand('build:web', async function (args, ...p) {
-    if (!args.module) {
-      return build.fn(args, ...p)
+    if (args.module === undefined) {
+      await legacyBuild({ ...args }, ...p)
+      await moduleBuild({ ...args }, ...p)
     } else {
-      args.module = false
-      await build.fn(args, ...p)
-      process.env.VUE_CLI_MODERN_BUILD = true
-      process.env.VUE_CLI_MODERN_MODE = true
-      await build.fn(args, ...p)
+      return build.fn(args, ...p)
     }
   })
 }
