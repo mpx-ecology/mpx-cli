122baa43db103d1850475df84190cdc1461f5d92
Mater <bxh8640@gmail.com>
2022-04-14 11:20:47 +0800

    fix: [vue-cli-plugin-mpx-web] watch:web => serve:web

--- packages/vue-cli-plugin-mpx-web/generator/index.js
+++ packages/vue-cli-plugin-mpx-web/generator/index.js
@@ -2,7 +2,7 @@ module.exports = function (api, options) {
   api.render('./template', options)
   api.extendPackage({
     scripts: {
-      'watch:web': 'mpx-cli-service serve:web',
+      'serve:web': 'mpx-cli-service serve:web',
       'build:web': 'mpx-cli-service build:web'
     }
   })
