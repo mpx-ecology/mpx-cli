066a137e2108957897e7267b1d9c2a391f63b619
Mater <bxh8640@gmail.com>
2022-05-17 17:41:50 +0800

    fix: add ts extensions

--- packages/vue-cli-plugin-mpx-eslint/index.js
+++ packages/vue-cli-plugin-mpx-eslint/index.js
@@ -3,7 +3,7 @@ const ESLintPlugin = require('eslint-webpack-plugin')
 module.exports = function (api, options, webpackConfig) {
   api.chainWebpack(webpackConfig => {
     webpackConfig.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{
-      extensions: ['js', 'mpx']
+      extensions: ['js', 'ts', 'mpx']
     }])
   })
 }
