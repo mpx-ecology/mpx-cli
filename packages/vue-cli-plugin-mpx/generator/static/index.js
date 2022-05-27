4b92a844107ff22853c14ca18ccc07c88cd28d89
Mater <bxh8640@gmail.com>
2022-05-27 10:23:47 +0800

    fix: conflict

--- packages/vue-cli-plugin-mpx/generator/static/index.js
+++ packages/vue-cli-plugin-mpx/generator/static/index.js
@@ -1,16 +1,7 @@
-const supportedModes = require('../../config/supportedModes')
-
 module.exports = function (api, options) {
-  const srcMode = options.srcMode
-  const newOptions = {
+  api.render('.', {
     ...options,
     cloudFunc: !!options.cloudFunc,
     isPlugin: !!options.isPlugin
-  }
-  api.render(`./${srcMode}`, newOptions)
-  if (srcMode === 'wx' && !!options.cross) {
-    supportedModes.forEach(srcMode => {
-      api.render(`./${srcMode}`, newOptions)
-    })
-  }
+  })
 }
