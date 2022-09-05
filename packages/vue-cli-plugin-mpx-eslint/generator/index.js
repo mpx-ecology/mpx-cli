47d9dddb4c7aabb4b3de29ca85f9a8d581d6b41a
pagnkelly <632228198@qq.com>
2022-09-05 11:17:11 +0800

    feat: new eslint config

--- packages/vue-cli-plugin-mpx-eslint/generator/index.js
+++ packages/vue-cli-plugin-mpx-eslint/generator/index.js
@@ -5,21 +5,8 @@ module.exports = function (api, options = {}) {
     },
     devDependencies: {
       'eslint-webpack-plugin': '^3.1.1',
-      'babel-eslint': '^10.0.1',
       eslint: '^7.0.0',
-      'eslint-config-babel': '^8.0.2',
-      'eslint-config-standard': '^12.0.0',
-      'eslint-friendly-formatter': '^4.0.1',
-      'eslint-plugin-html': '^6.0.1',
-      'eslint-plugin-import': '^2.14.0',
-      'eslint-plugin-local-rules': '^0.1.0',
-      'eslint-plugin-node': '^8.0.0',
-      'eslint-plugin-prettier': '^2.6.2',
-      'eslint-plugin-promise': '^4.0.1',
-      'eslint-plugin-standard': '^4.0.0'
+      "@mpxjs/eslint-config": "^1.0.3"
     }
-  })
-  api.render('./template', {
-    needTs: !!options.needTs
   })
 }
--- packages/vue-cli-plugin-mpx-eslint/generator/template/_eslintrc.js
+++ packages/vue-cli-plugin-mpx-eslint/generator/template/_eslintrc.js
@@ -1,37 +1,21 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  extends: 'standard',
-  settings: {
-    'html/html-extensions': ['.html', '.mpx'],  // consider .html and .mpx files as HTML
-  },
-  plugins: [
-    'html'
-  ],
-  globals: {
-    wx: true,
-    getApp: true,
-    App: true,
-    __mpx_mode__: true
-  },
+  extends: ['@mpxjs'],
   rules: {
-    camelcase: ['error', { 'allow': ['__mpx_mode__'] }]
+    // .mpx文件规则 https://mpx-ecology.github.io/eslint-plugin-mpx/rules/
   },
-  <%_ if(needTs) { _%>
   overrides: [
     {
       files: ['**/*.ts'],
-      parser: '@typescript-eslint/parser',
-      extends: [
-        'standard',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      plugins: ['@typescript-eslint']
+      rules: {
+        // .ts文件规则 https://typescript-eslint.io/rules/
+        
+      }
+    },
+    {
+      files: ['**/*.js'],
+      rules: {
+        // .js文件规则 https://eslint.bootcss.com/docs/rules/
+      }
     }
   ]
-  <%_ } _%>
 }
