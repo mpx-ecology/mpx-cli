85f833c5e71319afbb08bedc9328aa30bc2c3268
pagnkelly <632228198@qq.com>
2022-11-25 01:03:51 +0800

    feat: test

--- scripts/test-prompts.js
+++ scripts/test-prompts.js
@@ -1,11 +1,6 @@
 module.exports = {
   name: 'test',
-  cross: true,
-  transWeb: false,
-  isPlugin: false,
   needTs: true,
   needUnitTest: false,
-  needE2ETest: true,
-  description: 'A mpx project',
-  appid: 'touristappid'
+  needE2ETest: true
 }
