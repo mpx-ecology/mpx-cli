983982106516676e3d2e6f15f68e54b75337f5d3
pagnkelly <632228198@qq.com>
2022-12-02 14:59:04 +0800

    feat: jest config reseason

--- packages/vue-cli-plugin-mpx-unit-test/generator/index.js
+++ packages/vue-cli-plugin-mpx-unit-test/generator/index.js
@@ -6,10 +6,20 @@ module.exports = function (api, options = {}) {
     }
   })
 
+  api.extendPackage({
+    devDependencies: {
+      '@mpxjs/mpx-jest': '^0.0.9',
+      '@mpxjs/miniprogram-simulate': '^1.4.8',
+      'babel-jest': '^25.3.0',
+      jest: '^24.9.0'
+    }
+  })
+
   if (needTs) {
     api.extendPackage({
       devDependencies: {
-        'ts-jest': '^27.1.2'
+        'ts-jest': '^27.1.2',
+        '@types/jest': '^27.5.1'
       }
     })
   }
