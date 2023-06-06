7cb558d07f3d39ee0487cb7d971d6727602b1939
hiyuki <674883329@qq.com>
2023-06-06 15:47:26 +0800

    fix: update webpack plugin version

--- packages/vue-cli-plugin-mpx-utility-first-css/generator/index.js
+++ packages/vue-cli-plugin-mpx-utility-first-css/generator/index.js
@@ -4,8 +4,7 @@ module.exports = function (api, options) {
       windicss: '^3.5.6',
       'windicss-webpack-plugin': '^1.7.8',
       '@mpxjs/windicss-base': '^2.8.28-beta.6',
-      '@mpxjs/windicss-plugin': '^2.8.28-beta.8',
-      '@mpxjs/webpack-plugin': '^2.8.28-beta.8'
+      '@mpxjs/windicss-plugin': '^2.8.28-beta.8'
     },
     vue: {
       pluginOptions: {
