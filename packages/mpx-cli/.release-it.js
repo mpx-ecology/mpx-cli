1f286ca94c77c9dcd6b1866af322f9246e6402c5
Mater <bxh8640@gmail.com>
2023-06-09 13:37:53 +0800

    feat: release-cli

---
+++ packages/mpx-cli/.release-it.js
@@ -0,0 +1,13 @@
+module.exports = {
+  git: {
+    commitMessage: 'chore: release v${version}'
+  },
+  github: {
+    release: true
+  },
+  npm: {
+    skipChecks: true
+  },
+  plugins: {},
+  hooks: {}
+}
