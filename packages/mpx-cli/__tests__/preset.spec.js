e4182b4b46686f8d660728f9b4ae235313c7527c
yaojianxin <yaojianxin_i@didiglobal.com>
2023-08-02 10:05:28 +0800

    fix: 单测

--- packages/mpx-cli/__tests__/preset.spec.js
+++ packages/mpx-cli/__tests__/preset.spec.js
@@ -1,5 +1,5 @@
 jest.mock('inquirer')
-
+const glob = require('glob')
 const path = require('path')
 const fs = require('fs')
 const create = require('@mpxjs/cli/lib/create')
@@ -228,10 +228,9 @@ test('test-e2e', async () => {
   expect(pkg.devDependencies).toHaveProperty('@mpxjs/vue-cli-plugin-mpx-e2e-test')
 })
 
-// 测试一下 test-yao-1.0.0 这个项目，有没有 @mpxjs/babel-plugin-inject-page-events 这个依赖
-test('test-yao-1.0.0', async () => {
+// 封装一个方法，用来生成项目，然后将配置项作为参数传入
+const createProject = async (name, options) => {
   const cwd = path.resolve(__dirname, '../../test')
-  const name = 'test-yao-1.0.0'
   await create(
     name,
     {
@@ -239,108 +238,152 @@ test('test-yao-1.0.0', async () => {
       git: false,
       cwd
     },
-    {
-      srcMode: 'wx',
-      appid: 'test-yao-1.0.0',
-      description: 'test',
-      cross: true,
-      needE2ETest: true,
-      plugins: {},
-      useConfigFiles: true
-      // srcMode: 'wx' 表示小程序的类型是微信小程序
-      // appid: 'test-yao-1.0.0' 表示小程序的appid
-      // description: 'test' 表示小程序的描述
-      // cross: true 表示是否跨平台
-      // needE2ETest: true 表示是否需要e2e测试
-      // plugins: {} 表示插件
-      // useConfigFiles: true 表示是否使用配置文件
-    }
+    options
+    // srcMode: 'wx' 表示小程序的类型是微信小程序
+    // appid: 'test-yao-1.0.0' 表示小程序的appid
+    // description: 'test' 表示小程序的描述
+    // cross: true 表示是否跨平台
+    // needE2ETest: true 表示是否需要e2e测试
+    // plugins: {} 表示插件
+    // useConfigFiles: true 表示是否使用配置文件
   )
+}
 
-  const pkg = require(path.resolve(cwd, name, 'package.json'))
-  expect(pkg.devDependencies).toHaveProperty('@mpxjs/babel-plugin-inject-page-events')
-})
-
-// 将srcMode改成ali，测试一下 test-yao-1.0.1 这个项目生成的static/ali目录是否存在，还是否存在wx目录
-test('test-yao-1.0.1', async () => {
+// wx模式
+test('test-wx', async () => {
   const cwd = path.resolve(__dirname, '../../test')
-  const name = 'test-yao-1.0.1'
-  await create(
-    name,
-    {
-      force: true,
-      git: false,
-      cwd
-    },
-    {
-      srcMode: 'ali',
-      appid: 'test-yao-1.0.1',
-      description: 'test',
-      cross: true,
-      needE2ETest: true,
-      plugins: {},
-      useConfigFiles: true
-    }
-  )
-
+  const name = 'test-wx'
+  await createProject(name, {
+    srcMode: 'wx',
+    appid: 'test-plugin-have-appid',
+    description: 'test-plugin-have-description',
+    cross: true,
+    needE2ETest: true,
+    plugins: {},
+    useConfigFiles: true
+  })
+  // 生成的目录下是否包含了wx目录，ali目录，swan目录，tt目录，dd目录
   const staticWxDir = fs.existsSync(path.resolve(cwd, name, 'static/wx'))
   const staticAliDir = fs.existsSync(path.resolve(cwd, name, 'static/ali'))
-  expect(staticWxDir).toBe(false)
+  const staticSwanDir = fs.existsSync(path.resolve(cwd, name, 'static/swan'))
+  const staticQqDir = fs.existsSync(path.resolve(cwd, name, 'static/qq'))
+  const staticDdDir = fs.existsSync(path.resolve(cwd, name, 'static/dd'))
+  const staticTtDir = fs.existsSync(path.resolve(cwd, name, 'static/tt'))
+  expect(staticWxDir).toBe(true)
   expect(staticAliDir).toBe(true)
-  // a: 1. fs.existsSync(path.resolve(cwd, name, 'static/wx')) 判断static/wx目录是否存在，如果存在返回true，不存在返回false
-  // a: 2. fs.existsSync(path.resolve(cwd, name, 'static/ali')) 判断static/ali目录是否存在，如果存在返回true，不存在返回false
-  // a: 3. expect(staticWxDir).toBe(false) 判断staticWxDir是否为false，如果为false则测试通过，如果为true则测试失败
-  // a: 4. expect(staticAliDir).toBe(true) 判断staticAliDir是否为true，如果为true则测试通过，如果为false则测试失败
+  expect(staticSwanDir).toBe(true)
+  expect(staticQqDir).toBe(false)
+  expect(staticDdDir).toBe(true)
+  expect(staticTtDir).toBeTruthy()
+
+  // 生成的appid是否是test-plugin-have-appid
+  const projectConfig = require(path.resolve(cwd, name, 'static/wx/project.config.json'))
+  expect(projectConfig.appid).toBe('test-plugin-have-appid')
+
+  // 生成的description是否是test-plugin-have-description
+  expect(projectConfig.description).toBe('test-plugin-have-description')
+
+  // 保存快照
+  // 检查生成的目录是否和预期的目录一致
+  expect({
+    staticWxDir,
+    staticAliDir,
+    staticSwanDir,
+    staticQqDir,
+    staticDdDir,
+    staticTtDir
+  }).toMatchSnapshot()
+
+  // 检查生成的目录下的project.config.json文件是否和预期的一致
+  expect(projectConfig).toMatchSnapshot()
 })
 
-// 测试一下 test-yao-1.0.0 这个项目，有没有 @mpxjs/babel-plugin-inject-page-events 这个依赖
-test('test-yao-vue-1.0.0', async () => {
+// 测试插件是否存在,wx模式
+test('test-plugin-have', async () => {
   const cwd = path.resolve(__dirname, '../../test')
-  const name = 'test-yao-vue-1.0.0'
-  await create(
-    name,
-    {
-      force: true,
-      git: false,
-      cwd
-    },
-    {
-      srcMode: 'wx',
-      appid: 'test-yao-vue-1.0.0',
-      description: 'test',
-      cross: true,
-      needE2ETest: true,
-      plugins: {},
-      useConfigFiles: true
-    }
-  )
+  const name = 'test-plugin-have'
+  await createProject(name, {
+    srcMode: 'wx',
+    appid: 'test-plugin-have-appid',
+    description: 'test-plugin-have-description',
+    cross: true,
+    needE2ETest: true,
+    plugins: {},
+    useConfigFiles: true
+  })
 
+  // 有没有 @mpxjs/babel-plugin-inject-page-events 这个依赖
   const pkg = require(path.resolve(cwd, name, 'package.json'))
   expect(pkg.devDependencies).toHaveProperty('@mpxjs/babel-plugin-inject-page-events')
 })
 
-// 将srcMode改成ali，测试一下 test-yao-1.0.1 这个项目生成的static/ali目录是否存在，还是否存在wx目录,是否还有 @mpxjs/babel-plugin-inject-page-events 这个依赖
-test('test-yao-vue-1.0.1', async () => {
+// 将srcMode改成ali，生成的static/ali目录是否存在，还是否存在wx目录
+test('test-ali', async () => {
   const cwd = path.resolve(__dirname, '../../test')
-  const name = 'test-yao-vue-1.0.1'
-  await create(
-    name,
-    {
-      force: true,
-      git: false,
-      cwd
+  const name = 'test-ali'
+  await createProject(name, {
+    srcMode: 'ali',
+    appid: 'test-ali-appid',
+    description: 'test-ali-description',
+    cross: true,
+    needE2ETest: true,
+    plugins: {},
+    useConfigFiles: true
+  })
+
+  const staticWxDir = glob.sync(path.resolve(cwd, name, 'static/wx'))
+  const staticAliDir = glob.sync(path.resolve(cwd, name, 'static/ali'))
+  expect(staticWxDir.length).toBe(0) // 确保 static/wx 目录不存在
+  expect(staticAliDir.length).toBeGreaterThan(0) // 确保 static/ali 目录存在
+
+  // 保存快照
+  // 检查生成的目录是否和预期的目录一致
+  expect({
+    staticWxDir,
+    staticAliDir
+  }).toMatchSnapshot()
+})
+
+// 插件组合测试
+test('test-plugin-combine', async () => {
+  const cwd = path.resolve(__dirname, '../../test')
+  const name = 'test-plugin-combine'
+  await createProject(name, {
+    srcMode: 'wx',
+    appid: 'test-plugin-combine-appid',
+    description: 'test-plugin-combine-description',
+    cross: true,
+    needE2ETest: true,
+    plugins: {
+      '@mpxjs/vue-cli-plugin-mpx': {
+        version: '^2.0.0'
+      },
+      '@mpxjs/vue-cli-plugin-mpx-typescript': {
+        version: '^2.0.0'
+      },
+      '@mpxjs/vue-cli-plugin-mpx-unit-test': {
+        version: '^2.0.0'
+      },
+      '@mpxjs/vue-cli-plugin-mpx-e2e-test': {
+        version: '^2.0.0'
+      }
     },
-    {
-      srcMode: 'ali',
-      appid: 'test-yao-vue-1.0.1',
-      description: 'test',
-      cross: true,
-      needE2ETest: true,
-      plugins: {},
-      useConfigFiles: true
-    }
-  )
+    useConfigFiles: true
+  })
 
   const pkg = require(path.resolve(cwd, name, 'package.json'))
-  expect(pkg.devDependencies).toHaveProperty('@mpxjs/babel-plugin-inject-page-events')
+  expect(pkg.devDependencies).toHaveProperty('@mpxjs/vue-cli-plugin-mpx')
+  expect(pkg.devDependencies).toHaveProperty('@mpxjs/vue-cli-plugin-mpx-typescript')
+  expect(pkg.devDependencies).toHaveProperty('@mpxjs/vue-cli-plugin-mpx-unit-test')
+  expect(pkg.devDependencies).toHaveProperty('@mpxjs/vue-cli-plugin-mpx-e2e-test')
+
+  // 查看上面五个插件的版本是否正确
+  expect(pkg.devDependencies['@mpxjs/vue-cli-plugin-mpx']).toBe('^2.0.0')
+  expect(pkg.devDependencies['@mpxjs/vue-cli-plugin-mpx-typescript']).toBe('^2.0.0')
+  expect(pkg.devDependencies['@mpxjs/vue-cli-plugin-mpx-unit-test']).toBe('^2.0.0')
+  expect(pkg.devDependencies['@mpxjs/vue-cli-plugin-mpx-e2e-test']).toBe('^2.0.0')
+
+  // 保存快照
+  // 检查生成的文件是否和预期的一致
+  expect(pkg).toMatchSnapshot()
 })
