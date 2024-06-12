4792bd423581f2925a66425767a4ab9c6c33730f
mater <bxh8640@gmail.com>
2024-06-12 10:43:24 +0800

    feat: support lintOnSave option

--- packages/vue-cli-plugin-mpx-eslint/index.js
+++ packages/vue-cli-plugin-mpx-eslint/index.js
@@ -1,9 +1,28 @@
 const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = function (api, options, webpackConfig) {
-  api.chainWebpack(webpackConfig => {
-    webpackConfig.plugin('eslint-webpack-plugin').use(ESLintPlugin, [{
-      extensions: ['js', 'ts', 'mpx']
-    }])
-  })
+  if (options.lintOnSave) {
+    api.chainWebpack((webpackConfig) => {
+      const { lintOnSave } = options
+      const extensions = ['js', 'ts', 'mpx']
+      const treatAllAsWarnings = lintOnSave === true || lintOnSave === 'warning'
+      const treatAllAsErrors = lintOnSave === 'error'
+
+      const failOnWarning = treatAllAsErrors
+      const failOnError = !treatAllAsWarnings
+
+      /** @type {import('eslint-webpack-plugin').Options & import('eslint').ESLint.Options} */
+      const eslintWebpackPluginOptions = {
+        // common to both plugin and ESlint
+        extensions,
+        // ESlint options
+        failOnWarning,
+        failOnError
+      }
+
+      webpackConfig
+        .plugin('eslint-webpack-plugin')
+        .use(ESLintPlugin, [eslintWebpackPluginOptions])
+    })
+  }
 }
