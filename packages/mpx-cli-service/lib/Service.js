38787be4dbf9f70c0972aa4abddc354ddbab1e38
Mater <bxh8640@gmail.com>
2024-12-17 17:15:05 +0800

    feat: 支持异步注入配置 (#97)

--- packages/mpx-cli-service/lib/Service.js
+++ packages/mpx-cli-service/lib/Service.js
@@ -1,8 +1,32 @@
 const Service = require('@vue/cli-service')
-
 const { getServerBundle } = require('@mpxjs/cli-shared-utils')
+const Config = require('webpack-chain')
+const PluginAPI = require('@vue/cli-service/lib/PluginAPI')
 
 process.env.CI = 't'
+
+Service.prototype.resolveChainableWebpackConfig = function () {
+  const chainableConfig = new Config()
+  // apply chains
+  this.webpackChainFns.forEach(fn => {
+    const res = fn(chainableConfig)
+    if (res instanceof Promise) {
+      this.webpackChainPromises = this.webpackChainPromises || []
+      this.webpackChainPromises.push(res)
+    }
+  })
+  return chainableConfig
+}
+
+const originResolveWebpackConfig = PluginAPI.prototype.resolveWebpackConfig
+PluginAPI.prototype.resolveWebpackConfig = async function (chainableConfig) {
+  chainableConfig = chainableConfig || this.resolveChainableWebpackConfig()
+  if (this.service.webpackChainPromises) {
+    await Promise.all(this.service.webpackChainPromises)
+  }
+  const res = originResolveWebpackConfig.call(this, chainableConfig)
+  return res
+}
 
 module.exports = Service
 
