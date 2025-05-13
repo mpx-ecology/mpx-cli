1be82cf81a40ca83aead08f3a35fd70a872714cf
mater <bxh8640@gmail.com>
2025-05-13 20:08:04 +0800

    fix: comment

--- packages/vue-cli-plugin-mpx-eslint/index.js
+++ packages/vue-cli-plugin-mpx-eslint/index.js
@@ -9,7 +9,7 @@ module.exports = function (api, options, webpackConfig) {
       const eslintWebpackPluginOptions = {
         // common to both plugin and ESlint
         extensions,
-        // ESlint options 全部禁用
+        // ESlint options
         // 5.x的eslint-webpack-plugin这两个选项是控制是否退出构建的，但是eslint错误上不应该退出构建，所以这里全部禁用
         failOnWarning: false,
         failOnError: false,
