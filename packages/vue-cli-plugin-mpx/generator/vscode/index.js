bffdce874070f17652c1a4fe689743fd8af15a26
Soon Wang <wangshunnn@gmail.com>
2025-07-18 10:49:24 +0800

    chore: update

--- packages/vue-cli-plugin-mpx/generator/vscode/index.js
+++ packages/vue-cli-plugin-mpx/generator/vscode/index.js
@@ -1,7 +1,7 @@
 /**
  * VSCode IDE 工作区配置
  * - unocss 插件支持
- * - Mpx 新版本插件：Mpx (Official)
+ * - VS Code 插件推荐：Mpx (Official) ..
  */
 module.exports = function (api, options) {
   api.render('./template', options)
