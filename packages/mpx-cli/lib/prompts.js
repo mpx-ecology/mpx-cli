7032fd3374000fe9811efb38ba36b6355c2fcedd
hiyuki <hiyuki.mio@gmail.com>
2026-03-04 13:43:03 +0800

    update prompts desc

--- packages/mpx-cli/lib/prompts.js
+++ packages/mpx-cli/lib/prompts.js
@@ -5,28 +5,28 @@ module.exports = [
     name: 'srcMode',
     type: 'list',
     required: true,
-    message: '请选择小程序项目所属平台（目前仅微信下支持跨平台输出）',
+    message: '请选择项目源码模式（仅微信模式支持跨平台输出）',
     choices: ['wx', 'ali', 'swan', 'qq', 'tt', 'dd'],
     default: 'wx'
   },
   {
     name: 'cross',
     when: ({ srcMode }) => srcMode === 'wx',
-    message: '是否需要跨小程序平台',
+    message: '是否需要跨平台输出其他小程序和 Web',
     type: 'confirm',
     default: true
   },
   {
     name: 'needRn',
     when: ({ srcMode }) => srcMode === 'wx',
-    message: '是否需要输出react-native',
+    message: '是否需要跨平台输出 React Native',
     type: 'confirm',
     default: false
   },
   {
     name: 'needSSR',
     when: ({ srcMode, cross }) => srcMode === 'wx' && cross === true,
-    message: '是否需要 web ssr',
+    message: '是否需要使用 Web SSR',
     type: 'confirm',
     default: false,
     preset: {
@@ -56,7 +56,7 @@ module.exports = [
     when: ({ srcMode, cross, cloudFunc }) => srcMode === 'wx' && cross === false && cloudFunc === false,
     type: 'confirm',
     message:
-      '是否是个插件项目?（不清楚请选 No ！什么是插件项目请看微信官方文档！）',
+      '是否是小程序插件项目?（不清楚请选 No ！什么是插件项目请看微信官方文档！）',
     default: false,
     preset: {
       plugins: {
@@ -69,7 +69,7 @@ module.exports = [
   {
     name: 'needEslint',
     type: 'confirm',
-    message: '是否需要eslint',
+    message: '是否需要使用 Eslint',
     default: true,
     preset: {
       plugins: {
@@ -82,8 +82,8 @@ module.exports = [
   {
     name: 'needTs',
     type: 'confirm',
-    message: '是否需要typescript',
-    default: false,
+    message: '是否需要使用 Typescript',
+    default: true,
     preset: {
       plugins: {
         [`${prefix}-typescript`]: {
@@ -95,7 +95,7 @@ module.exports = [
   {
     name: 'needUtilityFirstCSS',
     type: 'confirm',
-    message: '是否需要使用原子类',
+    message: '是否需要使用原子CSS',
     default: false,
     preset: {
       plugins: {
@@ -107,7 +107,7 @@ module.exports = [
   },
   {
     name: 'needUnitTest',
-    message: '是否需要单元测试',
+    message: '是否需要使用单元测试',
     type: 'confirm',
     default: false,
     preset: {
@@ -120,7 +120,7 @@ module.exports = [
   },
   {
     name: 'needE2ETest',
-    message: '是否需要自动化测试',
+    message: '是否需要使用E2E测试',
     type: 'confirm',
     default: false,
     preset: {
