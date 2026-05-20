cfa62e46972c775ab7d2fcdefd1e973b10df0c3c
mackwang <mackwang@didiglobal.com>
2026-05-20 14:31:46 +0800

    补充最新依赖信息

--- packages/vue-cli-plugin-mpx/config/base.js
+++ packages/vue-cli-plugin-mpx/config/base.js
@@ -427,7 +427,8 @@ function addRnWebpackConfig (api, options, config, target) {
     'react-native-get-location': 'react-native-get-location',
     'react-native-linear-gradient': 'react-native-linear-gradient',
     'react-native-haptic-feedback': 'react-native-haptic-feedback',
-    'react-native-svg/css': 'react-native-svg/css'
+    'react-native-svg/css': 'react-native-svg/css',
+    'react-native-vision-camera': 'react-native-vision-camera',
   })
 }
 
